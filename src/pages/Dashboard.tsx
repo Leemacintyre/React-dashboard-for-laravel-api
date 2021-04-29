e581dd4e536bc92734a84060fa7ffdf57489b6ee
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-04-29 06:08:28 +0100

    added Register page and Wrapper component

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from '../components/Wrapper.jsx';
+
+const Dashboard = () => {
+  return (
+    <Wrapper>
+      Dashboard
+    </Wrapper>
+  );
+};
+
+export default Dashboard;
