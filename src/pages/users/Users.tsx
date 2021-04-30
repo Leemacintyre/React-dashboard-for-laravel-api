06f6fa8a8b500eee5af703afa680649f761bf116
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-04-30 07:20:12 +0100

    get request to display all users on dashboard and added pagination

---
+++ src/pages/users/Users.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import Wrapper from '../../components/Wrapper';
+import axios from 'axios';
+import { User } from '../../models/user';
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0);
+
+  useEffect(() => {
+    (
+      async () => {
+        const { data } = await axios.get(`users?page=${page}`);
+
+        setUsers(data.data);
+        setLastPage(data.meta.last_page);
+        console.log(`users?page=${page}`)
+      }
+    )()
+  }, [page]);
+
+  const next = () => {
+    if (page < lastPage) {
+      setPage(page + 1);
+    }
+  }
+  const prev = () => {
+    if (page >= 1) {
+      setPage(page - 1)
+    }
+  }
+
+
+  return (
+    <Wrapper>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Action</th>
+          </tr>
+          </thead>
+          <tbody>
+          {
+            users.map((user: User) => {
+              return (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.first_name} {user.last_name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role.name}</td>
+                  <td>{user.role.id}</td>
+                </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+      </div>
+      <nav>
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" onClick={prev}>Previous</a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" onClick={next}>Next</a>
+          </li>
+        </ul>
+      </nav>
+    </Wrapper>
+
+  );
+};
+
+export default Users;
