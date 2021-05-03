95f3d643c78060f23a66ab170da5d3bab2628ae2
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-05-03 08:00:22 +0100

    added functionality to edit roles

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import UserCreate from './pages/users/UserCreate';
 import UserEdit from './pages/users/UserEdit';
 import Roles from './pages/roles/Roles';
 import RoleCreate from './pages/roles/RoleCreate';
+import RoleEdit from './pages/roles/RoleEdit';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path={'/users/:id/edit'} component={UserEdit}/>
         <Route path={'/roles'} exact component={Roles}/>
         <Route path={'/roles/create'}  component={RoleCreate}/>
+        <Route path={'/roles/:id/edit'}  component={RoleEdit}/>
 
       </BrowserRouter>
     </div>
