b5469b8dd3a9bd5d6b5e8327eb3682eee3c32c5c
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-05-04 07:09:59 +0100

    added ProductCreate page and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import RoleCreate from './pages/roles/RoleCreate';
 import RoleEdit from './pages/roles/RoleEdit';
 import Products from './pages/products/Products';
 import ProductCreate from './pages/products/ProductCreate';
+import ProductEdit from './pages/products/ProductEdit';
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
         <Route path={'/roles/:id/edit'}  component={RoleEdit}/>
         <Route path={'/products'} exact component={Products}/>
         <Route path={'/products/create'}  component={ProductCreate}/>
+        <Route path={'/products/:id/edit'}  component={ProductEdit}/>
 
       </BrowserRouter>
     </div>
